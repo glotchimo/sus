bafa7802487a7862c4a419b9612bed8ba06c5747
Elliott Maguire <glotchimo@pm.me>
2021-09-15 14:51:11 -0700

    add a simple shell for testing

--- src/monkey.c
+++ src/monkey.c
@@ -4,15 +4,45 @@
 #include "lexer.h"
 #include "token.h"
 
+void loop();
+char *read_line();
+
 int main(int argc, char *argv[]) {
-    char *test = (char *)malloc(15);
-    test = "let five = 5;\0";
+    fprintf(stdout, "--- libmonkey - v0.0.0 - test shell ---\n");
+    loop();
+}
 
-    Lexer *l = lexer_new(test);
+void loop() {
+    char *line;
 
-    Token t = lexer_next_token(l);
-    while (t.type != EoF) {
-        printf("%d ", t.type);
-        t = lexer_next_token(l);
+    do {
+        printf("🐒 ");
+        line = read_line();
+
+        Lexer *l = lexer_new(line);
+        Token t = lexer_next_token(l);
+        while (t.type != EoF) {
+            fprintf(stdout, "%d ", t.type);
+            t = lexer_next_token(l);
+        }
+        fprintf(stdout, "\n");
+
+        free(line);
+    } while (true);
+}
+
+char *read_line() {
+    char *line = NULL;
+    size_t bufsize = 0;
+
+    if (getline(&line, &bufsize, stdin) == -1) {
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS);
+        } else {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
     }
+
+    return line;
 }
